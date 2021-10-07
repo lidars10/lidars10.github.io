52d522fe859d859943e43c524a23d3b88bf7b510
lidars10 <92042850+lidars10@users.noreply.github.com>
2021-10-07 21:36:02 +0300

    first upload

---
+++ js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+$(document).ready(function () {
+
+    $(".note_link").on("mouseover", function () {
+        //$(this).css("display","none");
+    });
+
+});
